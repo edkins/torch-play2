957609a5f7587f49670ac6c1dc5c1a4d1dda9bbf
Giles Edkins <edkins@gmail.com>
2022-08-14 12:25:06 -0400

    Create tests and list them

--- serve.py
+++ serve.py
@@ -1,29 +1,63 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import jsonschema
 import os
 import re
+from traceback import print_exception
+from urllib.parse import urlparse, parse_qs
 
 from model import get_config, get_results
-from transparency import create_highly_activating_input
+from transparency import create_and_run_test, get_test
 
 re_model_name = re.compile(r'^[-_a-zA-Z0-9]+$')
 re_model = re.compile(r'^\/api\/model\/([-_a-zA-Z0-9]+)$')
 re_test = re.compile(r'^\/api\/model\/([-_a-zA-Z0-9]+)\/test$')
-re_test_filename = re.compile(r'^test-[-_a-zA-Z0-9]+\.yaml$')
+re_test_filename = re.compile(r'^(test-[-_a-zA-Z0-9,]+)\.yaml$')
+
+test_payload_schema = {
+    'type': 'object',
+    'properties': {
+        'layer': {'type':'string'},
+        'neuron': {'type':'string'}
+    },
+    'required': ['layer','neuron']
+}
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         if re_test.match(self.path):
             model_name = re_test.match(self.path)[1]
+            length = int(self.headers['content-length'])
+            payload_data = self.rfile.read(length)
+            try:
+                payload = json.loads(payload_data)
+                jsonschema.validate(instance=payload, schema=test_payload_schema)
+            except:
+                self.send_error(400)
+                return
+
+            try:
+                test_name = create_and_run_test(model_name, payload['layer'], payload['neuron'])
+            except Exception as e:
+                print_exception(e)
+                self.send_error(500)
+                return
+            self.send_response(204)
+            self.end_headers()
+        else:
+            self.send_error(404)
+
 
     def do_GET(self):
-        if self.path in ['/', '/index.html']:
+        o = urlparse(self.path)
+        path = o.path
+        if path in ['/', '/index.html']:
             with open('index.html','rb') as f:
                 self.send_response(200)
                 self.send_header('content-type','text/html')
                 self.end_headers()
                 self.wfile.write(f.read())
-        elif self.path == '/api/model':
+        elif path == '/api/model':
             model_names = []
             for entry in os.scandir('models'):
                 if entry.is_dir():
@@ -40,8 +74,8 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps({
                 'items': models
             }).encode('utf-8'))
-        elif re_model.match(self.path):
-            model_name = re_model.match(self.path)[1]
+        elif re_model.match(path):
+            model_name = re_model.match(path)[1]
             _,config = get_config(f'models/{model_name}/config.yaml')
             results = get_results(f'models/{model_name}/results.yaml')
             self.send_response(200)
@@ -52,17 +86,28 @@ class RequestHandler(BaseHTTPRequestHandler):
                     'layers': results['layers']
                 }
             }).encode('utf-8'))
-        elif re_test.match(self.path):
-            model_name = re_test.match(self.path)[1]
+        elif re_test.match(path):
+            query = parse_qs(o.query)
+            model_name = re_test.match(path)[1]
             test_names = []
             for entry in os.scandir(f'models/{model_name}/'):
-                if not entry.is_dir() and re_test_filename.match(entry.name):
-                    test_names.append(entry.name)
+                m = re_test_filename.match(entry.name)
+                if m:
+                    test_names.append(m[1])
             self.send_response(200)
             self.send_header('content-type','application/json')
             self.end_headers()
+            tests = []
+            for name in sorted(test_names):
+                if len(query) > 0:
+                    details = get_test(f'models/{model_name}/{name}.yaml')
+                    if 'layer' in query and details['layer'] != query['layer'][0]:
+                        continue
+                    if 'neuron' in query and details['neuron'] != query['neuron'][0]:
+                        continue
+                tests.append({'name': name, 'layer':details['layer'], 'neuron':details['neuron']})
             self.wfile.write(json.dumps({
-                'items': [{'name':name} for name in sorted(test_names)]
+                'items': tests
             }).encode('utf-8'))
         else:
             self.send_error(404)
--- transparency.py
+++ transparency.py
@@ -1,16 +1,52 @@
+from datetime import datetime
 import numpy as np
+import time
 import torch
+import yaml
 
-import configured_nn
+from configured_nn import ConfiguredNN
+from model import get_config, get_results
 
-def create_highly_activating_input(layers: list[dict], x_shape: tuple[int], layer: int, neuron: int) -> np.ndarray:
-    model = ConfiguredNN(layers[:layer+1], x_shape)
-    inp = torch.rand(x_shape, requires_grad=True)
-    opt = torch.optim.Adam([inp])
+def create_highly_activating_input(layers: list[dict], x_shape: tuple[int], layer: int, neuron: tuple[int], device: str) -> np.ndarray:
+    model = ConfiguredNN(layers[:layer+1], x_shape).to(device)
+    inp = torch.rand(*x_shape, requires_grad=True, device=device)
+    optimizer = torch.optim.Adam([inp])
+    model.eval()
     for i in range(10):
         model.zero_grad()
         optimizer.zero_grad()
         output_layer = model(inp)
-        output_layer[neuron].backward()
+        (-output_layer[neuron]).backward()
         optimizer.step()
     return inp.detach().to('cpu').numpy()
+
+def get_test(filename: str):
+    with open(filename) as f:
+        text = f.read()
+        return yaml.safe_load(text)
+
+def parse_neuron(neuron: str) -> tuple[int]:
+    return tuple(int(x) for x in neuron.split(','))
+
+def create_and_run_test(model: str, layer: str, neuron: str) -> str:
+    _, config = get_config(f'models/{model}/config.yaml')
+    results = get_results(f'models/{model}/results.yaml')
+    print(f'Creating highly activating input for model {model}, layer {layer}, neuron {neuron}')
+    start_time = time.monotonic()
+    inp = create_highly_activating_input(config['layers'], results['x_shape'], int(layer), parse_neuron(neuron), config['training']['device'])
+    time_taken = time.monotonic() - start_time
+    print(f'Created highly activating input for model {model}, layer {layer}, neuron {neuron}')
+    date = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
+    test_name = f'test-{layer}-{neuron}-{date}'
+    test_result = {
+        'name': test_name,
+        'x_shape': results['x_shape'],
+        'data': inp.tolist(),
+        'model': model,
+        'layer': layer,
+        'neuron': neuron,
+        'time_taken': time_taken
+    }
+    filename = f'models/{model}/{test_name}.yaml'
+    with open(filename, 'w') as f:
+        f.write(yaml.dump(test_result))
